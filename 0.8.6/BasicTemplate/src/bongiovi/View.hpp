80ad9a208e36b85a9803bc3b92e55cf2f0ba22d2
yiwenl <bongiovi015@gmail.com>
2016-04-02 11:54:50 +0100

    simplified view
    Signed-off-by: yiwenl <bongiovi015@gmail.com>

--- 0.8.6/BasicTemplate/src/bongiovi/View.hpp
+++ 0.8.6/BasicTemplate/src/bongiovi/View.hpp
@@ -21,18 +21,41 @@ using namespace std;
 namespace bongiovi {
     class View {
         public:
-        View();
-        View(string, string);
+        
+        View() {
+            _vsPath = "shaders/basic.vert";
+            _fsPath = "shaders/basic.frag";
+            
+            _initShader();
+        };
+        
+        View(string vsPath, string fsPath) : _vsPath(vsPath), _fsPath(fsPath) {
+            _initShader();
+        };
+        
         gl::GlslProg*   shader;
         gl::VboMesh     mesh;
         string          _vsPath;
         string          _fsPath;
         
-        void    render();
-        
         private:
-        void    _init();
-        void    _initShader();
+        
+        void    _initShader() {
+            
+            try {
+                shader = new ci::gl::GlslProg( loadResource(_vsPath), loadResource(_fsPath) );
+            }
+            catch( gl::GlslProgCompileExc &exc ) {
+                console() << "Cannot compile shader: " << exc.what() << std::endl;
+                exit(1);
+            }
+            catch( Exception &exc ){
+                console() << "Cannot load shader: " << exc.what() << std::endl;
+                exit(1);
+            }
+            cout << "Creating shader program : " << _vsPath << ", " << _fsPath << endl;
+
+        }
     };
 }
 
