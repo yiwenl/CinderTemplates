34a384e7604c0aaaf7c15b98b3a2211126ca0848
yiwenl <bongiovi015@gmail.com>
2016-04-02 14:37:47 +0100

    to add ViewBall Helper
    Signed-off-by: yiwenl <bongiovi015@gmail.com>

--- 0.8.6/BasicTemplate/src/BasicTemplateApp.cpp
+++ 0.8.6/BasicTemplate/src/BasicTemplateApp.cpp
@@ -4,6 +4,7 @@
 #include "bongiovi/Scene.hpp"
 #include "bongiovi/ViewAxis.hpp"
 #include "bongiovi/ViewDotsPlane.hpp"
+#include "bongiovi/ViewBall.hpp"
 #include "bongiovi/OrbitalControl.hpp"
 #include "ViewTest.hpp"
 
@@ -26,7 +27,7 @@ class BasicTemplateApp : public AppNative {
     Scene* _scene;
     ViewAxis* _vAxis;
     ViewDotsPlane* _vDotsPlane;
-    
+    ViewBall*   _vBall;
     ViewTest*   _vTest;
 };
 
@@ -48,6 +49,7 @@ void BasicTemplateApp::setup()
     
     _vAxis = new ViewAxis();
     _vDotsPlane = new ViewDotsPlane();
+    _vBall = new ViewBall();
     
     _vTest = new ViewTest();
 }
@@ -63,12 +65,21 @@ void BasicTemplateApp::update()
 void BasicTemplateApp::draw()
 {
 	// clear out the window with black
+    time += 0.05;
 	gl::clear( Color( 0, 0, 0 ) );
     gl::setMatrices(*camera);
     _vAxis->render();
     _vDotsPlane->render();
     
+    _vBall->position.x = cos(time) * 2;
+    _vBall->position.z = sin(time) * 2;
+    
+    float scale = cos(time*0.5) * .5 + .5;
+    scale = .1 + scale * .2;
+    _vBall->scale.set(scale, scale, scale);
+    
     _vTest->render();
+    _vBall->render();
 }
 
 CINDER_APP_NATIVE( BasicTemplateApp, RendererGl )
